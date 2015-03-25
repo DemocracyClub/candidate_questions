a99150d5c98074bde9218d6feb68f4cb200a0e4c
Edward Betts <edward@4angle.com>
2015-03-25 14:06:19 +0000

    Use URLs from the prototype app

--- q_and_a/urls.py
+++ q_and_a/urls.py
@@ -13,7 +13,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^organisations', include('organisations.urls')),
-    url(r'^$', TemplateView.as_view(template_name="home.html"), name='home'),
-    # url(r'^', include('prototype.urls')),
+    url(r'^', include('prototype.urls')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
