01e16ca7decaa1cb314e399c619cd7809e07dadd
Edward Betts <edward@4angle.com>
2015-03-26 16:12:00 +0000

    Don't import models that no longer exist.

--- q_and_a/apps/prototype/admin.py
+++ q_and_a/apps/prototype/admin.py
@@ -1,7 +1,1 @@
 from django.contrib import admin
-
-from .models import (Organisation, Question, Answer)
-
-admin.site.register(Organisation)
-admin.site.register(Question)
-admin.site.register(Answer)
