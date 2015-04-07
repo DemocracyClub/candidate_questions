3f77c3cbcb93df7ff80b5daa825853386f2058da
Daniel Ramsay <daniel@dretzq.org.uk>
2015-04-07 21:54:00 +0100

    Use proper settings import

--- q_and_a/apps/questions/management/commands/assign_questions.py
+++ q_and_a/apps/questions/management/commands/assign_questions.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 
-from q_and_a.settings import OPEN_QUESTION_TARGET
+from django.conf import settings
 from candidates.models import Candidate
 from questions.models import Answer,Question
 
@@ -15,10 +15,10 @@ class Command(BaseCommand):
             self.stdout.write("Assigning questions for: {}".format(candidate))
             # get open question count for candidate
             open_questions = candidate.get_open_question_count()
-            if open_questions >= OPEN_QUESTION_TARGET:
+            if open_questions >= settings.OPEN_QUESTION_TARGET:
                 continue
             # find questions that have not already been assigned to this candidate
-            new_question_count = OPEN_QUESTION_TARGET - open_questions
+            new_question_count = settings.OPEN_QUESTION_TARGET - open_questions
 
             candidate.assign_questions(new_question_count)
                 
