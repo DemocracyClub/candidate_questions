dc6aaeeb9ac9a0806fa64bf394a0e355f4defa8c
Edward Betts <edward@4angle.com>
2015-04-09 12:34:43 +0100

    Add candidate admin search fields.

--- q_and_a/apps/candidates/admin.py
+++ q_and_a/apps/candidates/admin.py
@@ -4,5 +4,6 @@ from .models import Candidate
 class CandidateAdmin(admin.ModelAdmin):
     list_display = ('name', 'contact_address', 'constituency_name', 'party')
     list_filter = ('party', )
+    search_fields = ('name', 'constituency_name', 'party')
 
 admin.site.register(Candidate, CandidateAdmin)
