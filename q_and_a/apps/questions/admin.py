70173e9a31b8e4ef5a6110e7535fa3e8e8a8ae8c
Edward Betts <edward@4angle.com>
2015-04-09 15:04:15 +0100

    Add more detail and filters to answers admin

--- q_and_a/apps/questions/admin.py
+++ q_and_a/apps/questions/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Question, Answer
 
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('question', 'candidate', 'answer', 'completed')
+    list_filter = ('question__question', 'completed')
+    search_fields = ('question__question', 'answer', 'candidate__name')
+
 admin.site.register(Question)
-admin.site.register(Answer)
+admin.site.register(Answer, AnswerAdmin)
