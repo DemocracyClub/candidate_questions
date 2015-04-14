ccedbb5d7186ecfa192eaff0723c1316faa8f57d
Edward Betts <edward@4angle.com>
2015-04-14 15:13:35 +0100

    Admin: show orgnisation asking the question

--- q_and_a/apps/questions/admin.py
+++ q_and_a/apps/questions/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from .models import Question, Answer
 
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question', 'organisation')
+
 class AnswerAdmin(admin.ModelAdmin):
     list_display = ('question', 'candidate', 'answer', 'completed')
     list_filter = ('question__question', 'completed')
     search_fields = ('question__question', 'answer', 'candidate__name')
 
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Answer, AnswerAdmin)
